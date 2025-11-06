70c1445e1592b16d1bfa656797d10d500cd4520f
Joan <70104624+itsjovdev@users.noreply.github.com>
2025-11-07 00:01:01 +0100

    feat: se agrega datos al objeto car y mostramos los datos

--- 1-car-sample.py
+++ 1-car-sample.py
@@ -1,7 +1,7 @@
 #Para empezar esta seria de guia de PPO, vamos a crear una clase simple llamada Car que representara un coche con atributos basicos como marca, modelo y año.
 
 class Car:
-    #El método constructor __init__ es el constructor: se ejecuta al crear un nuevo objeto
+    #El constructor __init__ es el constructor: se ejecuta al crear un nuevo objeto
     def __init__(self, manufacturer = '', model = None, color = '', cylinder = 0.00):
         self.manufacturer = manufacturer
         self.model = model
@@ -9,6 +9,14 @@ class Car:
         self.cylinder = cylinder
 
 car = Car()
+
+#Le pasamos datos al objeto car
+car.manufacturer = 'Toyota'
+car.model = 'Corolla'
+car.color = 'Red'
+car.cylinder = 1.8
+
+#Mostramos los datos del objeto car
 print(f'car.manufacturer: ',  car.manufacturer)
 print(f'car.model: ',  car.model)
 print(f'car.color: ',  car.color)
