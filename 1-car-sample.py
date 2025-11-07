e5722d793b0e45273728fa477b11a4136ef7ef5a
Joan <70104624+itsjovdev@users.noreply.github.com>
2025-11-08 00:05:45 +0100

    feat:implementando atributos privados y se explicara que como se pueden modificar

--- 1-car-sample.py
+++ 1-car-sample.py
@@ -3,20 +3,22 @@
 class Car:
     #El constructor __init__ es el constructor: se ejecuta al crear un nuevo objeto
     def __init__(self, manufacturer = '', model = None, color = '', cylinder = 0.00):
-        self.manufacturer = manufacturer
-        self.model = model
-        self.color = color
-        self.cylinder = cylinder
+        #Los atributos con doble guion bajo (__) son privados.
+        #Esto significa que no pueden ser accedidos ni modificados directamente desde fuera de la clase.
+        self.__manufacturer = manufacturer
+        self.__model = model
+        self.__color = color
+        self.__cylinder = cylinder
         
     #metodo para mostrar los detalles del coche
     #No es recomendable usar print dentro de los metodos, ya que deben centrarse en devolver datos, no en mostrarlos
     #Como en este metodo no imprime nada, para que se pueda devolver valores cuando le pasamos al objeto se necesitara usar print()
     #cuando lo llamemos desde el objeto para mostrar su valor en consola.
     def details(self):
-        detail = f'manufacturer = {self.manufacturer}\n'
-        detail += f'model = {self.model}\n'
-        detail += f'color = {self.color}\n'
-        detail += f'cylinder = {self.cylinder}\n'
+        detail = f'manufacturer = {self.__manufacturer}\n'
+        detail += f'model = {self.__model}\n'
+        detail += f'color = {self.__color}\n'
+        detail += f'cylinder = {self.__cylinder}\n'
         return detail
 
 #Creamos un objeto de la clase Car
@@ -27,6 +29,7 @@ car.manufacturer = 'Toyota'
 car.model = 'Corolla'
 car.color = 'Red'
 car.cylinder = 1.8
+
 #Mostramos los detalles del objeto car
 print(car.details()) 
 
